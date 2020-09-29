169df607143893dbb1fee2be5e1ef23e67dab022
ahadli <amirhossein.adli@gmail.com>
2020-09-29 20:37:00 +0330

    adds pictures table

---
+++ database/migrations/2020_09_29_164946_create_pictures_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('picture');
+            $table->string('default_picture');
+            $table->enum('state',['deactivate','active']);
+            $table->string('link')->unique();
+            $table->date('expire_time')->nullable()->default(NULL);
+            $table->integer('expire_count')->nullable()->default(NULL);
+            $table->string('token');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
