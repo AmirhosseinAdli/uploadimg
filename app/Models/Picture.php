9ad8acbefe0b115e06499c9cf0840bd2719d4191
ahadli <amirhossein.adli@gmail.com>
2020-09-29 20:51:25 +0330

    adds Models

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -9,4 +9,8 @@ class Picture extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id','picture','default_picture','state','link','expire_time','expire_count','token'];
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
 }
