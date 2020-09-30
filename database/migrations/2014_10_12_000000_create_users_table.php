57ada91e1f6716011fd8e102ddb56c620926ebcb
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-09-30 10:58:06 +0200

    Improves some fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['admin','uploader']);
+            $table->enum('role',['admin','normal']);
             $table->rememberToken();
             $table->timestamps();
         });
