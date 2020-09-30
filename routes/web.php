0d2e982aa56f4d8d6e97d57ee4cfb5a4667108a1
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-09-30 18:26:32 +0200

    improves tables

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+<<<<<<< HEAD
+=======
+
+Route::resource('/token','TokenController');
+>>>>>>> create-viewer-image
