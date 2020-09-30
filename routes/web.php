c8d37aa71c95139922638a25feadc1e5acf86262
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-09-30 15:43:47 +0200

    Adds new table

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 
 Route::get('/', function () {
     return view('welcome');
