f3505366499a4ff3fd29532fc9f8822506bd50bc
omid <omidkh7900@gamil.com>
2020-10-06 20:43:09 +0330

    user dashboard

--- app/Http/Controllers/Panel/HomeController.php
+++ app/Http/Controllers/Panel/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
+use App\Models\Picture;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -14,6 +16,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('panel.dashboard');
+        $total_image = Picture::all()->count();
+
+        $active_image = Picture::all()
+            ->where('state','=','active')
+            ->where('user_id','=',auth()->id());
+
+        $history = DB::table('histories')->distinct()->count('picture_id');
+
+        return view('panel.dashboard',compact('total_image','active_image','history'));
     }
 }
