82d7635d4861c536af1c8e11046beab0a62f385e
eagle13740 <eagle13740@gmail.com>
2020-10-06 21:43:44 +0330

    "test push"

--- resources/views/admin/histories/list.blade.php
+++ resources/views/admin/histories/list.blade.php
@@ -1,1 +1,35 @@
 @extends('layouts.admin')
+@section('content')
+
+
+    <div class="container">
+        <h1>History</h1><br><br><br>
+
+        <div class="container text-center">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover" >
+                    <thead>
+                    <tr>
+                        <th class="w-10"><h3>Username</h3></th>
+                        <th class="w-15"><h3>Pictalign-middleure</h3></th>
+                        <th class="w-15"><h3>Used Slug</h3></th>
+                        <th class="w-15"><h3>Used Token</h3></th>
+                        <th class="w-15"><h3>Visitor IP</h3></th>
+                        <th class="w-15"><h3>Opration</h3></th>
+                    </tr>
+                    </thead>
+                    <tr>
+                        <td>User1</td>
+                        <td><img src="" alt="" style="width: 100px ;height: 100px;"></td>
+                    </tr>
+                    <tr>
+                        <td>User2</td>
+                        <td><img src="" alt="" style="width: 100px ;height: 100px;"></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
--- resources/views/admin/histories/show.blade.php
+++ resources/views/admin/histories/show.blade.php
@@ -1,1 +1,34 @@
 @extends('layouts.admin')
+@section('content')
+
+
+    <div class="container">
+        <h1>[@Username]History</h1><br><br><br>
+
+        <div class="container text-center">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th class="w-20"><h3>Picture</h3></th>
+                        <th class="w-20"><h3>Used Slug</h3></th>
+                        <th class="w-20"><h3>Used Token</h3></th>
+                        <th class="w-20"><h3>Visitor IP</h3></th>
+                        <th class="w-20"><h3>Opration</h3></th>
+                    </tr>
+                    </thead>
+                    <tr>
+                        <td><img src="" alt="" style="width: 100px ;height: 100px;"></td>
+                        <td>Slug1</td>
+                    </tr>
+                    <tr>
+                        <td><img src="" alt="" style="width: 100px ;height: 100px;"></td>
+                        <td>Slug2</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
