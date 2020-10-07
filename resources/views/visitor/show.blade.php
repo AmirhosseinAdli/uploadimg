2831d3cc4a5f679fc49647577d01befa81e16d01
ImanST <sadafiiman@gmail.com>
2020-10-07 20:02:13 +0330

    add visitor view

---
+++ resources/views/visitor/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="offset-md-2 col-md-7">
+                <div class="form-inline form-group">
+                    <input type="email" class="form-control col-md-10 mr-3" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="token">
+                    <button type="submit" class="btn btn-primary ">submit</button>
+                </div>
+            </div>
+            <div class="offset-md-2 col-md-7">
+                <img src="https://apollo2.dl.playstation.net/cdn/UP4040/CUSA11461_00/FREE_CONTENT0iOHv5jHiSyMRChuFEkj/ShSwgzcI_PREVIEW_SCREENSHOT2_175613.jpg" alt="no image" class="thumbnail" width="100%" height="480px">
+            </div>
+        </div>
+    </div>
+
+@endsection
